c775354ffe440037aac5377c020e7392837bffcf
Nathan Manning <93832413+nmm001001@users.noreply.github.com>
2023-12-13 17:47:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,44 @@
-import networkx as nx
 import time
 from RCM import RCM
 from Fiedler import Fiedler
 import scipy.io as sio
 import os
+import scipy.sparse as ss
 
 # Generate seed For gnp_random_graph
-SEED = int(time.time())
+# SEED = int(time.time())
 
-# G = nx.gnp_random_graph(1000, .00001, SEED)
-# G = nx.gnp_random_graph(10000, .00001, SEED)
-# G = nx.gnp_random_graph(100000, .0001, SEED)
-# G = nx.gnp_random_graph(1000000, .0001, SEED)
+# G_1000 = ss.random(1000, 1000, density=0.0001, format='csr', random_state=42)
+# G_10000 = ss.random(10000, 10000, density=0.0001, format='csr', random_state=42)
+# G_100000 = ss.random(100000, 100000, density=0.00000001, format='csr', random_state=42)
+# G_1000000 = ss.random(1000000, 1000000, density=0.00000001, format='csr', random_state=42)
 
+# Load G
+G_1000 = ss.load_npz('data/G_1000.npz')
+# RCM_G_1000 = ss.load_npz('data/RCM_G_1000.npz')
+# Fiedler_G_1000 = ss.load_npz('data/Fiedler_G_1000.npz')
+# G_10000 = ss.load_npz('data/G_1000.npz')
+# RCM_G_10000 = ss.load_npz('data/RCM_G_1000.npz')
+# Fiedler_G_10000 = ss.load_npz('data/Fiedler_G_1000.npz')
 
-def get_neighbors(adjacency_matrix, node):
+def get_neighbors(csr_matrix, node):
 
-    if node < 0 or node >= len(adjacency_matrix):
+    if node < 0 or node >= csr_matrix.getnnz():
         # Node index out of bounds
-        return []
+        return
 
-    neighbors = []
-    for i in range(len(adjacency_matrix[node])):
-        if adjacency_matrix[[node], [i]] != 0:
-            neighbors.append(i)
-
-    return neighbors
+    start = csr_matrix.indptr[node]
+    end = csr_matrix.indptr[node+1]
+    return csr_matrix.indices[start:end]
 
 
 
 if __name__=='__main__':
-    
-    # RCM_G = RCM(G)
-    # Fiedler_G = Fiedler(G).todense()
+    G = G_1000
+    for i in range(G.getnnz()):
+        print(get_neighbors(G, i))
 
-    # # G = G.todense()
-    # GRAPH_SIZE = len(G)
 
-    # os.makedirs('data', exist_ok=True)
-    # A = nx.adjacency_matrix(G)
-
-    # sio.mmwrite(f'data/normal_{GRAPH_SIZE}.mtx', A)
-    # sio.mmwrite(f'data/Fiedler_{GRAPH_SIZE}.mtx', Fiedler_G)
-    # sio.mmwrite(f'data/RCM_{GRAPH_SIZE}.mtx', RCM_G)
-
-    G = sio.mmread('data/Fiedler_10000.mtx').todense()
 
     for i in range(len(G)):
         get_neighbors(G, i)
