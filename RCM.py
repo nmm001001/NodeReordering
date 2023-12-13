2a7a8c2930fb81c8622457fdbb5ead64ddca888f
Nathan Manning <93832413+nmm001001@users.noreply.github.com>
2023-12-13 17:08:34 -0500

    Update RCM.py

--- RCM.py
+++ RCM.py
@@ -13,21 +13,24 @@ def RCM(G):
     return: RCM_A -> Adjacency matrix with reverse Cuthill-Mckee node reordering -> type: matrix
     '''
 
-    if isinstance(G, np.ndarray):
-        G = nx.from_numpy_array(G)
+    # if isinstance(G, np.ndarray):
+    #     G = nx.from_numpy_array(G)
     # Convert sparse matrix to csr format & perform RCM reordering on nodes
 
-    csr_G = nx.to_scipy_sparse_array(G)
-    RCM_ordering = reverse_cuthill_mckee(csr_G)
+    # csr_G = nx.to_scipy_sparse_array(G)
+    RCM_ordering = reverse_cuthill_mckee(G)
 
     # If G is in csr format, convert to dense format, then calculate the adjacency matrix
-    if type(G) == scipy.sparse._csr.csr_array:
-        G = G.todense()
-    A = nx.adjacency_matrix(G)
+    # if type(G) == scipy.sparse._csr.csr_array:
+    #     G = G.todense()
+    # A = nx.from_scipy_sparse_array(G)
 
     # Reorder nodes according to RCM heuristic
-    RCM_A = A[RCM_ordering, :][:, RCM_ordering]
+    # RCM_A = G[np.ix_(RCM_ordering, RCM_ordering)]
+    RCM_G = G[RCM_ordering, RCM_ordering]
 
-    return RCM_A
+    return RCM_G 
+
+
 
 
