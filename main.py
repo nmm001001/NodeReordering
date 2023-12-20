391d2ad4bb764839a71cc3af71c2e7670fd6b28d
Nathan Manning <93832413+nmm001001@users.noreply.github.com>
2023-12-20 16:38:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import time
 from RCM import RCM
 from Fiedler import Fiedler
 import scipy.io as sio
-import os
 import scipy.sparse as ss
+from BPL import bits_per_link
 
 # Generate seed For gnp_random_graph
 # SEED = int(time.time())
@@ -14,31 +14,52 @@ import scipy.sparse as ss
 # G_1000000 = ss.random(1000000, 1000000, density=0.00000001, format='csr', random_state=42)
 
 # Load G
-G_1000 = ss.load_npz('data/G_1000.npz')
+# G_DATA = ss.load_npz('data/data.npz')
 # RCM_G_1000 = ss.load_npz('data/RCM_G_1000.npz')
 # Fiedler_G_1000 = ss.load_npz('data/Fiedler_G_1000.npz')
-# G_10000 = ss.load_npz('data/G_1000.npz')
-# RCM_G_10000 = ss.load_npz('data/RCM_G_1000.npz')
-# Fiedler_G_10000 = ss.load_npz('data/Fiedler_G_1000.npz')
+# G_10000 = ss.load_npz('/work/acslab/users/manning/489Project/data/G_10000_0000001.npz')
+# RCM_G_10000 = ss.load_npz('data/RCM_G_10000_0000001.npz')
+Fiedler_G_10000 = ss.load_npz('data/Fiedler_G_10000_0000001.npz')
 
 def get_neighbors(csr_matrix, node):
 
-    if node < 0 or node >= csr_matrix.getnnz():
+    if node < 0 or node >= csr_matrix.shape[0]:
         # Node index out of bounds
-        return
+        return []
 
     start = csr_matrix.indptr[node]
     end = csr_matrix.indptr[node+1]
     return csr_matrix.indices[start:end]
 
+def get_second_order_neighbors(csr_matrix, node):
+    second_order_neighbors = set()
+    first_order_neighbors = set(get_neighbors(csr_matrix, node))
+
+    for neighbor in first_order_neighbors:
+        # Exclude the original node and its first-order neighbors
+        neighbors_of_neighbor = set(get_neighbors(csr_matrix, neighbor))
+        second_order_neighbors.update(neighbors_of_neighbor)
+
+    # Remove the original node and its first-order neighbors
+    second_order_neighbors.difference_update(first_order_neighbors, {node})
+
+    return list(second_order_neighbors)
+    
 
 
 if __name__=='__main__':
-    G = G_1000
+
+    G = Fiedler_G_10000
+    print(len(G.nonzero()[0]))
+    print(bits_per_link(G))
+
+    start = time.time()
     for i in range(G.getnnz()):
-        print(get_neighbors(G, i))
+        get_second_order_neighbors(G, i)
+    end = time.time()
+
+    print(end - start)
 
 
 
-    for i in range(len(G)):
-        get_neighbors(G, i)
+
